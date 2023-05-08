1455778797506f058ee7efe89a28ad083df0a3de
Omar Samy <omar.samy3333@gmail.com>
2023-05-08 15:33:01 +0300

    replace fraction with fracty

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -5,7 +5,7 @@ import View from './View.js';
 import icons from 'url:../../img/icons.svg';
 
 //Use this to convert the decimal numbers to fractions
-import { Fraction } from 'fractional';
+import { fracty } from 'fracty';
 
 class RecipeView extends View {
   _parentElement = document.querySelector('.recipe');
@@ -138,7 +138,7 @@ class RecipeView extends View {
           <use href="${icons}#icon-check"></use>
         </svg>
         <div class="recipe__quantity">${
-          ing.quantity ? new Fraction(ing.quantity).toString() : '0'
+          ing.quantity ? new fracty(ing.quantity).toString() : '0'
         }</div>
         <div class="recipe__description">
           <span class="recipe__unit">${ing.unit}</span>
